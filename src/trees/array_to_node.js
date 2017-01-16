47059323688421248c27c09fe31f59780307e91d
THACHER <thachert1d@gmail.com>
2017-01-15 17:37:23 -0800

    trees exercises from cs-drills

--- src/trees/array_to_node.js
+++ src/trees/array_to_node.js
@@ -1,3 +1,39 @@
+/*
+  A tree can be represented by a single root node that has child nodes,
+  which in turn have child nodes etc...
+       A
+      /\
+    B   C
+   /\
+  D E
+  You can represent trees with just arrays in various different ways.
+  One way looks like this:
+  [
+    'a',
+    [
+      [
+        'b',
+        [
+          [
+           'd',
+           []
+         ],
+          [
+           'e',
+           []
+          ]
+        ]
+      ],
+      [
+        'c',
+        []
+      ]
+    ]
+  ]
+  Your job is to turn an array with the format above into a tree of nodes.
+  Imlement this depth-first, using recursion.
+*/
+
 var Node = require('./node');
 //Start on the above file- you'll want to use the class from that file!
 //It has properties already set up for you, such as 'name' and 'children'.
@@ -6,11 +42,7 @@ var Node = require('./node');
 
 //Make sure that this function calls new Node()
 var arrayToNode = function(input) {
-  var node = new Node(input[0]);
-  input[1].forEach(function (element) {
-    node.addChild(arrayToNode(element));
-  });
-  return node;
+
 };
 
 module.exports = arrayToNode;
